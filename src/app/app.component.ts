f93d88bc88d6fea7f7774f9f2da11fa5e73b3e5d
Alex Vong <avong2011@gmail.com>
2022-04-05 08:46:57 -0400

    feat: styling grid layout for surfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 
 @Component({
+  host: {
+    class: 'c-root',
+  },
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
