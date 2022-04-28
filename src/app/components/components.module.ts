0d3676201af8d63ee15771c45bc35339756043f1
Alex Vong <avong2011@gmail.com>
2022-04-28 08:58:45 -0400

    feat: added input and styled overlay

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MarqueeComponent} from './marquee/marquee.component';
 import {WelcomeComponent} from './welcome/welcome.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [MarqueeComponent, WelcomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [MarqueeComponent, WelcomeComponent],
 })
 export class ComponentsModule {}
--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,12 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {OverlayService} from '../overlay/overlay.service';
 
 @Component({
-  selector: 'app-welcome',
+  host: {class: 'c-welcome'},
+  selector: 'c-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WelcomeComponent implements OnInit {
-  constructor() {}
+  welcomeForm!: FormGroup;
+  constructor(private fb: FormBuilder, private render: Renderer2, private overlay: OverlayService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.welcomeForm = this.fb.group({
+      name: [''],
+    });
+
+    this.welcomeForm.valueChanges.subscribe((v) => {
+      console.log(v);
+    });
+  }
 }
